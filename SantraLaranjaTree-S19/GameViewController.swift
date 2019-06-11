ae96636ff965d695f2386684c7b61657d854eda2
rabbit <rabbit@eatcarrots.com>
2019-06-11 11:29:40 -0400

    setup blank template that is backwards compatible with old Xcode

--- SantraLaranjaTree-S19/GameViewController.swift
+++ SantraLaranjaTree-S19/GameViewController.swift
@@ -29,6 +29,10 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            
+            // show physics
+            view.showsPhysics = true
+            
         }
     }
 
