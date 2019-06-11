9c9ed7ae92ee502a18299249e9b6e042a096ae73
rabbit <rabbit@eatcarrots.com>
2019-06-11 11:38:05 -0400

    template for making an orange when you click

--- SantraLaranjaTree-S19/GameScene.swift
+++ SantraLaranjaTree-S19/GameScene.swift
@@ -12,9 +12,38 @@ import GameplayKit
 class GameScene: SKScene {
    
     
+    
     override func didMove(to view: SKView) {
    
     }
     
-   
+    func makeOrange(xPosition:CGFloat, yPosition:CGFloat) {
+        
+        // 1. create an orange sprite
+        let orange = SKSpriteNode(imageNamed: "Orange")
+        
+        // 2. set initial position of orange to be same
+        // as where mouse is clicked
+        
+        
+        // 3. set physics for the orange
+        // -- dyanmic = true
+        // -- gravity = true
+        // Both are true by default
+        
+        
+        // 4. Add the orange to the scene
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // make an orange
+        self.makeOrange(xPosition: CGFloat, yPosition: CGFloat)
+        
+    }
+    
+    
+    
+    
 }
